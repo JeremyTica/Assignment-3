07d4a77562c3223b6c7520d722ffa6427184133a
JeremyTica <jeremytica@gmail.com>
2023-11-25 22:52:55 -0500

    Implementation of the UML Framework

--- application.py
+++ application.py
@@ -2,3 +2,23 @@
 # 2023-12-1
 # Jeremy Tica
 
+class Account:
+    pass
+
+class SavingsAccount(Account):
+    pass
+
+class ChequingAccount(Account):
+    pass
+
+
+
+def run():
+    showMainMenu()
+    showAccountMenu()
+
+def showMainMenu():
+    pass
+
+def showAccountMenu():
+    pass
