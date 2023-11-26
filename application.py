0c20537093c55f2973da188cf430a7d7b9a5ee85
JeremyTica <jeremytica@gmail.com>
2023-11-25 23:20:22 -0500

    Defining the Getters and Setters
    This iteration observes the implementation of getters and setters, as well as other additions to the child classes

--- application.py
+++ application.py
@@ -3,13 +3,53 @@
 # Jeremy Tica
 
 class Account:
-    pass
+    def __init__(self, accountNumber, accountHolderName, rateOfInterest, currentBalance):
+        self.__accountNumber = accountNumber
+        self.__accountHolderName = accountHolderName
+        self.__rateOfInterest = rateOfInterest
+        self.__currentBalance = currentBalance
+
+    def getAccountNumber(self):
+        return self.__accountNumber
+
+    def getAccountHolderName(self):
+        return self.__accountHolderName
+
+    def getRateOfInterest(self):
+        return self.__rateOfInterest
+    
+    def getCurrentBalance(self):
+        return self.__currentBalance
+
+
+    def setAccountNumber(self, accountNumber):
+        self.__accountNumber = accountNumber
+
+    def setAccountHolderName(self, accountHolderName):
+        self.__accountHolderName = accountHolderName
+
+    def setRateOfInterest(self, rateOfInterest):
+        self.__rateOfInterest = rateOfInterest
+    
+    def setCurrentBalance(self, currentBalance):
+        self.__currentBalance = currentBalance
+
+
+    def deposit():
+        pass
+
+    def withdraw():
+        pass
+
 
 class SavingsAccount(Account):
-    pass
+    def __init__(self):
+        super().__init__()
+
 
 class ChequingAccount(Account):
-    pass
+    def __init__(self):
+        super().__init__()
 
 
 
